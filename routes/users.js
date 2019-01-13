1b9c3bd86dcd6e96d8b71dc316471066fbf6dffc
Andres Baumeler <andres.baumeler@gmail.com>
2019-01-13 21:59:58 +0100

    fixed require

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,4 @@
 /* eslint-disable object-curly-spacing */
-const express = require('express');
 const router = require('express-promise-router')();
 
 const { validateBody, schemas } = require('../helpers/routeHelpers');
