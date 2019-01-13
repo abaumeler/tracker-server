94641a5ca85c81867a040d78c1eb8fafd495f02c
Andres Baumeler <andres.baumeler@gmail.com>
2019-01-13 15:08:25 +0100

    added mongo db and email check against db

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
+const config = require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 var app = express();
 
 // MongoDB setup
+mongoose.Promise = global.Promise;
+
+const db_host = process.env.DB_HOST;
+const db_user = process.env.DB_USER;
+const db_password = process.env.DB_PW;
+
+mongoose.connect(`mongodb://${db_user}:${db_password}@${db_host}`, { useNewUrlParser: true });
 
 // Middlewares
 app.use(morgan('dev'));
